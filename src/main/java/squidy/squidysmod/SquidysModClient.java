1746151704c57e16b5b88e07498c630a4167cfd3
sta <stadoesyt@gmail.com>
2024-10-13 16:14:44 +0100

    Initial commit

---
+++ src/main/java/squidy/squidysmod/SquidysModClient.java
@@ -0,0 +1,10 @@
+package squidy.squidysmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SquidysModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
